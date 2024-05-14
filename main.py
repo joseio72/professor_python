d3bf29824dfd63d22750fe2fe7754d58b67cbbb9
ossorio <jossorio-pro@outlook.com>
2024-05-13 22:09:38 -0400

    entering toquestion promting

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # Professor Python
 # The purpose of this program is to test the retention of information.
-# Take a test on Indivdual Lessons
-# Take a test on all matiral
-# add new matiral
-# edit or remove old matiral
+# Take a test on all material
+# Track Scores on material
 
 import json
-import typer
+from os.path import isfile
+from re import sub
 from typing_extensions import Annotated
 import os
 import random
@@ -14,73 +13,74 @@ from rich.pretty import pprint
 from rich import print
 from datetime import datetime
 import shutil
-from typing import Optional
 from rich.columns import Columns
+from typing import Optional
 
-app = typer.Typer(no_args_is_help=True)
-
-MATERIAL = 'material.json'
-GLOSSARY = 'glossary.json'
-EXAM = 'CS0-003'
-CWD = os.getcwd()
-BACKUP_DESTINATION = '.bak'
-PWD_FILES = os.listdir(path='.')
-
-
-@app.command()
-def backup( file: Annotated[Optional[str], typer.Argument()] = None) -> None:
-    """
-    Command to backup the core matiral.
-    Name of the file will be matiral<UTC>.json
-    """
-    if not file: return
-    # Just check if the file is in the Dir
-    if not os.path.isfile(file):
-        print("File not found.")
-        return
-    try:
-        file_name = os.path.splitext(file)
-        # First move the cureent backup to the bak folder.
-        # Iterate through the files of the dir
-        utc_timestamp = str(datetime.utcnow().timestamp())
-        backup_file_name = file_name[0] + '_' + utc_timestamp + file_name[1]
-        backup_file_path = os.path.join(BACKUP_DESTINATION, backup_file_name)
-        print("Backup File:", backup_file_path)
-        # Last Create the new backup file.
-        shutil.copyfile(file, backup_file_path)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    return
-
-@app.command()
 def Challenge(
-        test_length: Annotated[Optional[int], typer.Argument()] = 10,
-        multiple_choice_number : Annotated[Optional[int], typer.Argument()] = 5
+        test_length: int  = 10,
+        multiple_choice_number : int = 5
         ):
     """
     argument determinds the length of the  test.
     test is comprised of learning material with Question and answer.
 
     """
-    # first we back up the File
-    backup(GLOSSARY)
-    # Goal is tp print one question at a time
-    # have a user give an answer sheet.
-    terms= None
-    with open(GLOSSARY, 'r+b') as jsonFile:
-        terms = json.load(jsonFile)
+    print(f'Test Length ${test_length}')
+    print(f'choice number ${multiple_choice_number}')
+    content = [i for i in os.listdir() if i.endswith('.json')]
 
-    term_keys = list(terms.keys())
-    term_values = list(terms.values())
+    #Right now th program does not care about memory saving
     score = None
-    correct_key = random.choice(term_keys)  # Randomly choose a key from the dictionary
     
-    correct_value = terms[question_key]  # Retrieve the value corresponding to the chosen key
-    #random_values_list = random.sample(term_values, k = multiple_choice_number - 1)
-    print(correct_key)
-    print(correct_value)
-    print(random_values_list)
+    for _ in range(test_length):
+        call_randit = lambda x: random.randint(0,len(x)-1)
+        randomContentIndex = call_randit(content)
+
+        selected_SubTopic = content[randomContentIndex]
+        print(selected_SubTopic)
+
+        sub_topic = None
+        with open(selected_SubTopic, 'r+b') as jsonFile:
+            sub_topic = json.load(jsonFile)
+
+        #  In this protion of the code base we are just taking the admin information out of the varible and giving it a # %%
+        #  Location  just to keep this apart in admin_headers. 
+        admin_headers = {key: sub_topic[key] for key in list(sub_topic.keys())[:6]}
+
+        # Then just removing them from the subtopic just to have some clean  Term and definitions.
+        sub_topic = {key: sub_topic[key] for key in list(sub_topic.keys())[6:]}
+
+        topic_keys = list(sub_topic.keys())
+        topic_values = list(sub_topic.values())
+        
+        #Coin flip to determine the term or defintion.
+        coin_flip = random.choice([True,False])
+        
+        question={
+            'prompt': '',
+            'choices':[],
+        }
+
+        if coin_flip: 
+            # terms
+            question['prompt'] = topic_keys[call_randit(topic_keys)]  # Randomly choose a key from the dictionary
+            #Now we all the choices
+            #First fill the bad choices
+            question['choices'] = []
+            random_index = random.randint(0, multiple_choice_number - 1)
+            question['choices'].insert(random_index, 6)
+        else: 
+            #Definitions
+            question['prompt'] = topic_values[call_randit(topic_values)]  # Randomly choose a key from the dictionary
+            #Now we all the choices
+            #First fill the bad choices
+            question['choices'] = []
+            random_index = random.randint(0, multiple_choice_number - 1)
+            question['choices'].insert(random_index, 6)
+
+        print(question)
+        print('\n')
     return
 
 if __name__ == "__main__":
-    app()
+    Challenge()
