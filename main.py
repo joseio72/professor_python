51c493de028229c01c37709932cdf9647b25caee
jOssorioRojas <jossorio-pro@outlook.com>
2024-05-17 09:48:41 -0400

    5-17

--- main.py
+++ main.py
@@ -6,7 +6,6 @@
 import json
 from os.path import isfile
 from re import sub
-from typing_extensions import Annotated
 import os
 import random
 from rich.pretty import pprint
@@ -14,73 +13,68 @@ from rich import print
 from datetime import datetime
 import shutil
 from rich.columns import Columns
-from typing import Optional
+
+CALL_RANDIT = lambda x: random.randint(0,len(x)-1)
+
+
+def randomSubTopicObject(selected_SubTopic):
+    sub_topic = None
+    with open(selected_SubTopic, 'r+b') as jsonFile:
+        sub_topic = json.load(jsonFile)
+    return sub_topic
+
 
 def Challenge(
-        test_length: int  = 10,
+        topic: dict = None,
         multiple_choice_number : int = 5
         ):
     """
-    argument determinds the length of the  test.
+    argument determinds the length of the  test and the content we are testing on
     test is comprised of learning material with Question and answer.
 
     """
-    print(f'Test Length ${test_length}')
-    print(f'choice number ${multiple_choice_number}')
-    content = [i for i in os.listdir() if i.endswith('.json')]
+    if not topic: return
 
-    #Right now th program does not care about memory saving
-    score = None
-    
-    for _ in range(test_length):
-        call_randit = lambda x: random.randint(0,len(x)-1)
-        randomContentIndex = call_randit(content)
+    admin_headers = {key: sub_topic[key] for key in list(topic.keys())[:6]}
 
-        selected_SubTopic = content[randomContentIndex]
-        print(selected_SubTopic)
+    content = {key: sub_topic[key] for key in list(topic.keys())[6:]}
 
-        sub_topic = None
-        with open(selected_SubTopic, 'r+b') as jsonFile:
-            sub_topic = json.load(jsonFile)
+    topic_keys = list(content.keys())
 
-        #  In this protion of the code base we are just taking the admin information out of the varible and giving it a # %%
-        #  Location  just to keep this apart in admin_headers. 
-        admin_headers = {key: sub_topic[key] for key in list(sub_topic.keys())[:6]}
+    topic_values = list(content.values())
 
-        # Then just removing them from the subtopic just to have some clean  Term and definitions.
-        sub_topic = {key: sub_topic[key] for key in list(sub_topic.keys())[6:]}
+    item={
+        'stem': '',
+        'key':'',
+        'alternatives':[],
+    }
 
-        topic_keys = list(sub_topic.keys())
-        topic_values = list(sub_topic.values())
-        
-        #Coin flip to determine the term or defintion.
-        coin_flip = random.choice([True,False])
-        
-        question={
-            'prompt': '',
-            'choices':[],
-        }
-
-        if coin_flip: 
-            # terms
-            question['prompt'] = topic_keys[call_randit(topic_keys)]  # Randomly choose a key from the dictionary
-            #Now we all the choices
-            #First fill the bad choices
-            question['choices'] = []
-            random_index = random.randint(0, multiple_choice_number - 1)
-            question['choices'].insert(random_index, 6)
-        else: 
-            #Definitions
-            question['prompt'] = topic_values[call_randit(topic_values)]  # Randomly choose a key from the dictionary
-            #Now we all the choices
-            #First fill the bad choices
-            question['choices'] = []
-            random_index = random.randint(0, multiple_choice_number - 1)
-            question['choices'].insert(random_index, 6)
-
-        print(question)
-        print('\n')
-    return
+    #if random.choice([True,False]):
+    used_indexs = []
+    if True: 
+        index = CALL_RANDIT(topic_keys)
+        print(index)
+        item['stem'] = topic_keys[index]  
+        item['key'] = topic_values[index] 
+        i = 0
+        while i < multiple_choice_number:
+            alt_index =  CALL_RANDIT(topic_values)
+            if index == alt_index or alt_index in used_indexs: 
+                print(used_indexs)
+                continue
+            item['alternatives'].append(topic_values[alt_index])
+            used_indexs.append(alt_index)
+            i+=1 
+    return item , admin_headers
 
 if __name__ == "__main__":
-    Challenge()
+
+    score = 0 
+    test_length = 10
+    for _ in range(test_length):
+        content = [i for i in os.listdir() if i.endswith('.json')]
+        randomContentIndex = CALL_RANDIT(content)
+        sub_topic = randomSubTopicObject(content[randomContentIndex])
+        item, source = Challenge(sub_topic)
+        print(source)
+        print(item)
