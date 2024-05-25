78b6c5e421071b4efaef563250bfe89d3a97c3e4
jOssorioRojas <jossorio-pro@outlook.com>
2024-05-25 19:27:18 -0400

    just a recurring save shell

--- bk_file.sh
+++ bk_file.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
+# Function to create a backup
+create_backup() {
+    local file="$1"
+    local backup_file="$file.bak"
+    cp "$file" "$backup_file"
+    echo "Backup created: $backup_file"
+}
+
+# Check if no arguments are provided
+if [ $# -eq 0 ]; then
+    # Backup files fisj and dpg
+    create_backup "./glossary.json"
+    create_backup "./highScore.json"
+    exit 0
+fi
+
 # Check if an argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <filename>"
+if [ $# -eq 1 ]; then
+    file="$1"
+
+    # Check if the file exists
+    if [ ! -f "$file" ]; then
+        echo "File $file does not exist."
+        exit 1
+    fi
+
+    # Create a backup of the provided file
+    create_backup "$file"
+else
+    echo "Usage: $0 [<filename>]"
     exit 1
 fi
-
-# Check if the file exists
-if [ ! -f "$1" ]; then
-    echo "File $1 does not exist."
-    exit 1
-fi
-
-# Create a backup filename with .bak extension
-backup_file="$1.bak"
-
-# Copy the original file to the backup file
-cp "$1" "$backup_file"
-
-echo "Backup created: $backup_file"
 
